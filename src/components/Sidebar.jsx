3a65bfd00bafd527b1e0f31aedb0ad4ab27aa2f0
BOUDEFFAZAKARYA <boudeffazack@gmail.com>
2022-10-28 22:03:11 +0100

    Detail Pin , Create Pin

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,6 +3,7 @@ import { NavLink, Link } from 'react-router-dom';
 import { RiHomeFill } from 'react-icons/ri';
 import { IoIosArrowForward } from 'react-icons/io';
 import logo from '../assets/logo.png';
+import { categories } from '../utils/data';
 
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize';
@@ -11,16 +12,7 @@ const Sidebar = ({ closeToggle, user }) => {
   const handleCloseSidebar = () => {
     if (closeToggle) closeToggle(false);
   };
-  const categories =[
-    {name:"CSS"},
-    {name:"java"},
-    {name:"react js"},
-    {name:"node js"},
-    {name:"flutter"},
 
-
-
-  ]
 
 
   
--- src/utils/data.js
+++ src/utils/data.js
@@ -1,3 +1,130 @@
+export const categories = [
+  {
+    name: 'CSS',
+    image: 'https://i.pinimg.com/750x/eb/47/44/eb4744eaa3b3ccd89749fa3470e2b0de.jpg',
+  },
+  {
+    name: 'java',
+    image: 'https://i.pinimg.com/236x/25/14/29/251429345940a47490cc3d47dfe0a8eb.jpg',
+  },
+  {
+    name: 'react js',
+    image: 'https://i.pinimg.com/236x/03/48/b6/0348b65919fcbe1e4f559dc4feb0ee13.jpg',
+  },
+  {
+    name: 'node js',
+    image: 'https://i.pinimg.com/750x/66/b1/29/66b1296d36598122e6a4c5452b5a7149.jpg',
+  },
+  {
+    name: 'flutter',
+    image: 'https://i.pinimg.com/236x/72/8c/b4/728cb43f48ca762a75da645c121e5c57.jpg',
+  },
+  {
+    name: 'HTML',
+    image: 'https://i.pinimg.com/236x/7d/ef/15/7def15ac734837346dac01fad598fc87.jpg',
+  },
+  {
+    name: 'Dart',
+    image: 'https://i.pinimg.com/236x/b9/82/d4/b982d49a1edd984c4faef745fd1f8479.jpg',
+  },
+  
+  {
+    name: 'others',
+    image: 'https://i.pinimg.com/236x/2e/63/c8/2e63c82dfd49aca8dccf9de3f57e8588.jpg',
+  },
+];
+
+
+
+
+export const pinDetailMorePinQuery = (pin) => {
+  const query = `*[_type == "pin" && category == '${pin.category}' && _id != '${pin._id}' ]{
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      _key,
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+
+
+
+export const userCreatedPinsQuery = (userId) => {
+  const query = `*[ _type == 'pin' && userId == '${userId}'] | order(_createdAt desc){
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+export const userSavedPinsQuery = (userId) => {
+  const query = `*[_type == 'pin' && '${userId}' in save[].userId ] | order(_createdAt desc) {
+    image{
+      asset->{
+        url
+      }
+    },
+    _id,
+    destination,
+    postedBy->{
+      _id,
+      userName,
+      image
+    },
+    save[]{
+      postedBy->{
+        _id,
+        userName,
+        image
+      },
+    },
+  }`;
+  return query;
+};
+
+
+
+
+
+
+
+
+
+
 export const userQuery = (userId) => {
     const query = `*[_type == "user" && _id == '${userId}']`;
     return query;
@@ -56,4 +183,41 @@ export const userQuery = (userId) => {
         },
       } `
      ;
-  
+
+
+     export const pinDetailQuery = (pinId) => {
+      const query = `*[_type == "pin" && _id == '${pinId}']{
+        image{
+          asset->{
+            url
+          }
+        },
+        _id,
+        title, 
+        about,
+        category,
+        destination,
+        postedBy->{
+          _id,
+          userName,
+          image
+        },
+       save[]{
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+        },
+        comments[]{
+          comment,
+          _key,
+          postedBy->{
+            _id,
+            userName,
+            image
+          },
+        }
+      }`;
+      return query;
+    };
